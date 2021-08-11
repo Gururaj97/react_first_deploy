6a57e570526514eabb9ce6cb6b90a1da593a415d
Gururaj Gidaveer <gururajgidaveer@Gururajs-MacBook-Air.local>
2021-08-11 19:34:37 +0530

    new image added to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import background from './images';
 
 function App() {
   return (
     <div className="App">
-    Hello World!...
+    <img src={background}></img>
     </div>
   );
 }
